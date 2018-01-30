372f22ee792a9d8d01921b0d4bc45420556fb4c1
PowerMtest <powermtest@gmail.com>
2018-01-30 23:16:22 +0100

    Refaktoring gotowych rozwiazan. Trzeba teraz uzyc metod statystycznych do policzenia najczesciej wystepujacych losowan

--- iterations/Losowanie6z49.cs
+++ iterations/Losowanie6z49.cs
@@ -25,53 +25,58 @@ namespace iterations
             LosowanieMetdaGlowna.LosowanieLiczb(losowanie);
             ArrayList listaLosowan = new ArrayList();
             var losowanieJakoTablica = new int[6];
-
+            var czasStart = DateTime.Now;
             
 
-            //Sprawdzamy czy w losowaniu nie ma zduplikowanych liczb -- nieskuteczne
-            #region Prawdopodobnie_zbedne_do_usuniecia
-            //!!!nieskuteczna metoda usuwania duplikatow
-            //for (int k = 0; k < losowanie.Length - 1; k++)
+            Console.WriteLine("Start aplikacji: "+czasStart);
+            #region Wyswietlanie_na_ekranie_pierwszego_losowania
+            ////do usuniecia
+            //for (int i = 0; i < (losowanie.Length); i++)
             //{
-            //    if (losowanie[k] == losowanie[k + 1]) //spr czy w nastepnej iteracji nie pojawia sie taka sama liczba
-            //    {
-            //        //Console.WriteLine("znalazlem duplikaty, losujemy powtornie");
-            //        LosowanieMetdaGlowna.LosowanieLiczb(losowanie);
-            //        k = 0;
-            //        //Console.WriteLine("nowe wylosowane liczby to: {0}", string.Join(", ", losowanie));
-            //    }
+            //    Console.WriteLine("{0}: {1} ", i + 1, string.Join(", ", losowanie[i]));
+            //    //Console.WriteLine("{0}: {1} ({2:p1})", i + 1, losowanie[i], (double)results[i] / (double)totalRolls);
+            //}
 
+            //foreach (var VARIABLE in losowanie)
+            //{
+            //    var i = 0;
+            //    losowanieJakoTablica[i] = VARIABLE;
+            //    i++;
             //} 
             #endregion
-            
-            #region Wyswietlanie_na_ekranie_pierwszego_losowania
-            //do usuniecia
-            for (int i = 0; i < (losowanie.Length); i++)
-            {
-                Console.WriteLine("{0}: {1} ", i + 1, string.Join(", ", losowanie[i]));
-                //Console.WriteLine("{0}: {1} ({2:p1})", i + 1, losowanie[i], (double)results[i] / (double)totalRolls);
-            }
 
-            foreach (var VARIABLE in losowanie)
-            {
-                var i = 0;
-                losowanieJakoTablica[i] = VARIABLE;
-                i++;
-            } 
+            IloscLosowanNaListe(losowanie, listaLosowan, out var i);
+
+            Console.WriteLine("Koniec obliczen: "+(DateTime.Now.Second-czasStart.Second) +" sekund");
+
+            #region Wypisz_wszystkie_wyniki_losowania
+            //to pozwala wypisac wszystkie wyniki
+            //Console.WriteLine("-------");
+            //foreach (int[] VARIABLE in listaLosowan)
+            //{
+            //    foreach (var i in VARIABLE)
+            //    {
+            //        Console.Write(i + ", ");
+            //    }
+
+            //    Console.WriteLine();
+            //} 
             #endregion
 
+            Console.WriteLine("Czas wykonania calosci programu: " + (DateTime.Now.Second - czasStart.Second) + " sekund");
 
-            //losowanie odpowiedniej ilosci liczb
-            //uniemozliwienie istnienia duplikatow w losowaniu (ponownie)
-            for (int i = 0; i < 100; i++)
+        }
+
+        private static void IloscLosowanNaListe(int[] losowanie, ArrayList listaLosowan, out int i)
+        {
+            for (i = 0; i < 1000; i++)
             {
                 var test = losowanie.AsEnumerable().ToArray();
                 listaLosowan.Add(test);
                 LosowanieMetdaGlowna.LosowanieLiczb(losowanie);
-                var rozne = losowanie.Distinct();//do uniemozliwienia istnienia duplikatow w losowaniu
+                var rozne = losowanie.Distinct(); //do uniemozliwienia istnienia duplikatow w losowaniu
                 if (rozne.ToArray().Length == losowanie.Length)
                 {
-                    
                     continue;
                 }
                 else
@@ -79,24 +84,7 @@ namespace iterations
                     listaLosowan.RemoveAt(i);
                     i--;
                 }
-
             }
-
-            #region Wypisz_wszystkie_wyniki_losowania
-            //to pozwala wypisac wszystkie wyniki
-            Console.WriteLine("-------");
-            foreach (int[] VARIABLE in listaLosowan)
-            {
-                foreach (var i in VARIABLE)
-                {
-                    Console.Write(i + ", ");
-                }
-
-                Console.WriteLine();
-            } 
-            #endregion
-
-
         }
     }
 }
